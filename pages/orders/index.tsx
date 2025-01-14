721c28dec44fdb618e8217a81f90289d52d8fceb
Imtiaz Laptop <imteiaz@gmail.com>
2025-01-14 23:08:29 +0000

    7:11

--- pages/orders/index.tsx
+++ pages/orders/index.tsx
@@ -1,123 +1,96 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import Container from '@mui/material/Container';
-import { GetStaticProps, NextPage } from 'next/types';
-import { getCustomers } from '../api/customers';
-import { useRouter } from 'next/router';
-import { Customer, Order } from '../customers';
-import { ObjectId } from 'mongodb';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { GetStaticProps, NextPage } from "next/types";
+import { getCustomers } from "../api/customers";
 
-const columns: GridColDef[] = [
-    {
-        field: 'id',
-        headerName: 'Order ID',
-        width: 120,
-    },
-    {
-        field: 'customerId',
-        headerName: 'Customer ID',
-        width: 150,
-    },
-    {
-        field: 'customerName',
-        headerName: 'Customer',
-        width: 150,
-        editable: true,
-    },
-    {
-        field: 'description',
-        headerName: 'Description',
-        type: 'string',
-        width: 400,
-        editable: true,
-    },
-    {
-        field: 'orderPrice',
-        headerName: 'Price',
-        type: 'number',
-        sortable: true,
-        width: 160,
-    },
+const columns: GridColDef<(typeof rows)[number]>[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
 ];
 
-interface OrderRow extends Order {
-    orderPrice: Number;
-    customerName: string;
-    customerId?: ObjectId;
-    id: ObjectId;
-}
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
 
-type Props = {
-    orders: Order[];
-};
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
+
+export const getStaticProps: GetStaticProps = async () => {
     const data = await getCustomers();
-
-    let orders: OrderRow[] = [];
-
-    data.forEach((customer: Customer) => {
-        if (customer.orders) {
-            customer.orders.forEach((order: Order) => {
-                orders.push({
-                    ...order,
-                    customerName: customer.name,
-                    customerId: customer._id,
-                    id: order._id,
-                    orderPrice: Number(order.price.$numberDecimal),
-                });
-            });
-        }
-    });
-    return {
-        props: {
-            orders: orders,
-        },
-        revalidate: 60,
-    };
+    
+    let orders: any = [];
+    
+data.forEach((customer) => {
+    if (customer.orders) {
+        customer.orders.forEach((order) => {})
+        console.log(orders)
+    }
+    
+})
+    
+  return {
+    props: {
+      orders: orders,
+    //     .map((customer) => {
+    //       return customer.orders || null;
+    //     })
+    //     .flat(1)
+    //     .filter((order) => {
+    //       return order != null; 
+    //     }),
+    // },
+    revalidate: 60,
+  }
 };
 
-const Orders: NextPage<Props> = (props) => {
-    const { customerId } = useRouter().query;
-
-    return (
-        <Container>
-            <Box sx={{ height: 400, width: '100%' }}>
-                <DataGrid
-                    filterModel={{
-                        items: [
-                            {
-                                columnField: 'customerId',
-                                operatorValue: 'equals',
-                                value: customerId,
-                            },
-                        ],
-                    }}
-                    rows={props.orders}
-                    columns={columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                    disableSelectionOnClick
-                    experimentalFeatures={{ newEditingApi: true }}
-                    initialState={{
-                        filter: {
-                            filterModel: {
-                                items: [
-                                    {
-                                        columnField: 'customerId',
-                                        operatorValue: 'equals',
-                                        value: customerId,
-                                    },
-                                ],
-                            },
-                        },
-                    }}
-                />
-            </Box>
-        </Container>
-    );
+const Orders: NextPage = (props) => {
+  console.log(props);
+  return (
+    <Box sx={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </Box>
+  );
 };
 
-export default Orders;
+export default Orders
